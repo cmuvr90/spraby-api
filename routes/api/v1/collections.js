4cef8caa1590ed97aaea75790db994a979ce09c8
yauheni.s <yauheni.s@spur-i-t.com>
2023-02-26 19:25:03 +0100

    add collections

--- routes/api/v1/collections.js
+++ routes/api/v1/collections.js
@@ -8,4 +8,7 @@ import {auth} from '../../../middlewares';
 export default function (router) {
   router.get('/collections/list', auth, CollectionController.index);
   router.get('/collections/:handle', auth, CollectionController.get);
+  router.post('/collections/save', auth, CollectionController.create);
+  router.put('/collections/:id/save', auth, CollectionController.update);
+  router.delete('/collections/:id', auth, CollectionController.delete);
 }
