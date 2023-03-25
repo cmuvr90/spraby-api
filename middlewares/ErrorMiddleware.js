e961d00596870fc3a7e87351b94f831a6a4695cb
yauheni.s <yauheni.s@spur-i-t.com>
2023-03-25 12:01:21 +0100

    use auto creating unique handle for products

--- middlewares/ErrorMiddleware.js
+++ middlewares/ErrorMiddleware.js
@@ -2,27 +2,37 @@ import LoginError from '../services/ErrorService/LoginError';
 import RegisterError from '../services/ErrorService/RegisterError';
 import AuthError from '../services/ErrorService/AuthError';
 import {TYPES} from '../ioc/types';
+import mongoose from 'mongoose';
 
 const ErrorMiddleware = async (err, req, res, next) => {
   const log = req.getService(TYPES.LogService);
 
-  if (err instanceof LoginError) {
-    await log.createLogger('LoginError').error(err.message, err.code, err.errors);
-    return res.sendError(err.message, err.code, err.errors);
-  }
+  try {
+    if (err instanceof LoginError) {
+      await log.createLogger('LoginError').error(err.message, err.code, err.errors);
+      return res.sendError(err.message, err.code, err.errors);
+    }
 
-  if (err instanceof RegisterError) {
-    await log.createLogger('RegisterError').error(err.message, err.code, err.errors);
-    return res.sendError(err.message, err.code, err.errors);
-  }
+    if (err instanceof RegisterError) {
+      await log.createLogger('RegisterError').error(err.message, err.code, err.errors);
+      return res.sendError(err.message, err.code, err.errors);
+    }
 
-  if (err instanceof AuthError) {
-    await log.createLogger('AuthError').error(err.message, err.code, err.errors);
-    return res.sendError(err.message, err.code, err.errors);
-  }
+    if (err instanceof AuthError) {
+      await log.createLogger('AuthError').error(err.message, err.code, err.errors);
+      return res.sendError(err.message, err.code, err.errors);
+    }
 
-  await log.createLogger('Errors').error(err);
-  return res.sendError('Server error', 500, [err?.message ?? 'error']);
+    if (err instanceof mongoose.Error.ValidationError) {
+      const errors = err?.errors ? Object.values(err.errors).map(e => e['properties'].message) : ['Bad request'];
+      return res.sendError('Bad request', 400, errors);
+    }
+
+    throw Error(err);
+  } catch (e) {
+    await log.createLogger('Errors').error(err);
+    return res.sendError('Server error', 500, [err?.message ?? 'error']);
+  }
 }
 
 
--- models/Products.js
+++ models/Products.js
@@ -6,10 +6,10 @@ import Variants from './Variants';
 import {getHandle} from '../services/utilites';
 
 const FIELDS = {
-  brand: {type: mongoose.Schema.Types.ObjectId, ref: Brands},
-  category: {type: mongoose.Schema.Types.ObjectId, ref: Categories},
-  title: {type: String, required: true},
-  handle: {type: String, required: true, unique: true},
+  brand: {type: mongoose.Schema.Types.ObjectId, ref: Brands, required: 'Brand is required',},
+  category: {type: mongoose.Schema.Types.ObjectId, ref: Categories, required: 'Category is required'},
+  title: {type: String, required: 'Title is required'},
+  handle: {type: String, required: 'Handle is required'},
   description: {type: String, default: null},
   image: {type: String, default: null},
   variants: [
@@ -19,13 +19,44 @@ const FIELDS = {
 
 const Products = new Model(FIELDS);
 
-/**
- *
- */
+//VALIDATIONS
+Products.path('handle').validate(async function (value) {
+  try {
+    let handle = value;
+    const DELIMITER = '_';
+
+    const product = await mongoose.model('Products')
+    .findOne({$or: [{handle: new RegExp(`^(${value})(${DELIMITER}\\d*)$`, 'i')}, {handle: value}]})
+    .sort({createdAt: -1})
+    .select({handle: 1});
+
+    if (product?.handle?.length) {
+      const matched = product?.handle.match(new RegExp(`(${DELIMITER}\\d*)$`));
+      if (matched && matched[0]?.length && Number.isInteger(+matched[0].replace('_', ''))) {
+        handle = handle + DELIMITER + (+matched[0].replace('_', '') + 1);
+      } else {
+        handle = `${handle}_1`;
+      }
+    }
+    this.handle = handle;
+    return true;
+  } catch (e) {
+    return false
+  }
+}, 'Handle has been unique');
+
+//HOOKS
+Products.pre('validate', {document: true, query: true}, async function (next) {
+  if (!this?.handle?.length) this.handle = getHandle(this.title);
+  next();
+});
+
 Products.pre('deleteOne', {document: true, query: true}, async function () {
   const product = await this.model.findOne(this.getQuery());
   if (product?.variants) await Variants.deleteMany({_id: {$in: product.variants}})
 });
+
+//METHODS
 
 /**
  *
@@ -145,7 +176,6 @@ Products.statics.getDto = function (product) {
  */
 Products.statics.createProduct = async function (params) {
   const data = {
-    handle: getHandle(params.title),
     title: params.title,
     brand: params.brand,
     description: params.description,
