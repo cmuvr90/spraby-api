f28a6e3edce732390f1e0de1f5e4bf3cedbd1b8a
yauheni.s <yauheni.s@spur-i-t.com>
2023-06-18 23:05:17 +0200

    update

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.use(express.static('files'))
+app.enable('trust proxy')
 app.use(cors({
   origin: (origin, callback) => {
     if (typeof origin === 'undefined') {
